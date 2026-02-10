cb9aa2baff281535bebfb328f993530683ed042c
Sudarshan K O <kosudarshan15@gmail.com>
2026-02-10 16:40:45 +0530

    l

---
+++ let1.js
@@ -0,0 +1,28 @@
+let a=10;
+
+a=20;
+
+console.log(a);
+
+ if(true){
+
+	let a= 10;
+
+	console.log(a);
+
+}
+
+const obj = {name:"JS"};
+
+obj.name="Java Script";
+
+//Constant the content can be changeable.
+
+console.log(obj);
+
+1==1;
+5=="5";
+
+console.log(a===1);
+console.log(5===5);
+console.log("hello"==="hello");
